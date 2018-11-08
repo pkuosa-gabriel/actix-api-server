15cc30f49919a0890d1c3557b0a70a7437d6d47e
Gabriel Wu <qqbbnease1004@126.com>
2018-11-08 23:09:45 +0800

    feat: basic api implementation

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,59 @@
 extern crate actix_web;
-use actix_web::{server, App, HttpRequest};
-use std::env; 
+extern crate env_logger;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate serde_json;
 
-fn index(_req: &HttpRequest) -> &'static str {
-    "Hello world!"
+use actix_web::middleware::Logger;
+use actix_web::{server, App, HttpRequest, Json, Result};
+use std::env;
+
+#[derive(Deserialize, Serialize)]
+struct Poem {
+    title: String,
+    author: String,
+    contents: String,
+}
+
+fn post_poem(mut poem: Json<Poem>) -> Result<Json<Poem>> {
+    poem.title.push_str("_new");
+    Ok(poem)
+}
+
+fn get_poems(_req: &HttpRequest) -> Result<Json<Poem>> {
+    Ok(Json(Poem{
+        title: String::from("Hello"),
+        author: String::from("Mike"),
+        contents: String::from("Hello, Mike!"),
+    }))
 }
 
 fn main() {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
     let mut address = String::from("127.0.0.1:");
     match env::var("PORT") {
         Ok(port) => {
             println!("Port: {}", port);
             address.push_str(&port);
-        },
+        }
         Err(e) => {
             println!("Couldn't read PORT ({}), will use default port: 8080", e);
             address.push_str("8080")
         }
     };
-    server::new(|| App::new().resource("/", |r| r.f(index)))
-        .bind(address)
+    server::new(|| {
+        App::new()
+            .middleware(Logger::default())
+            .middleware(Logger::new("%a %{User-Agent}i"))
+            .prefix("/api/v1")
+            .resource("/poems", |r| {
+                r.get().f(get_poems);
+                r.post().with(post_poem);
+            })
+    }).bind(address)
         .unwrap()
         .run();
 }
